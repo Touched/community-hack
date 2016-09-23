4792ffc4c1533b57cab925ca7f9dcd56d883f250
Touched <jtouched@gmail.com>
2016-09-23 18:31:41 +0200

    Add hook documentation

--- patches/opponent_table.s
+++ patches/opponent_table.s
@@ -1,8 +1,10 @@
+	// Adjust the X coordinate of the enemy trainer sprites for
+	// multi battles
 	.org 0x08037D80
         ldr r0, =opponent_cmd07_x_hook|1
         bx r0
         .pool
 
-        // Finish
-        .org 0x08038DFC
-        nop
+        // TODO: Figure out this code
+        // .org 0x08038DFC
+        // nop
