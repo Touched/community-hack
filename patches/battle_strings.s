cb9c9542a7a34112bb8fee5b67817c3985fe855a
Touched <jtouched@gmail.com>
2016-09-23 15:05:47 +0200

    Add send out battle message

--- patches/battle_strings.s
+++ patches/battle_strings.s
@@ -1,6 +1,12 @@
-	// Trainer battle hook
+	// Add multi battle trainer introduction message
         .org 0x080D73B6
         bx r7
 
         .org 0x080D73C0
         .word battle_string_intro_hook|1
+
+        // Add multi battle trainer send out enemy Pokemon message
+        .org 0x080D7474
+        ldr r0, =battle_string_sendout_hook|1
+        bx r0
+        .pool
--- src/hooks/battle_strings.s
+++ src/hooks/battle_strings.s
@@ -1,3 +1,7 @@
+	@@ TODO: Add buffers x2e and x2f for the other TRAINER
+
+        .align 2
+
         .thumb
         .align 2
 
@@ -21,3 +25,32 @@ battle_string_intro_hook:
         .align 2
 string_multi_battle_intro:
         .string "{STRING 28} {STRING 29} and\n{STRING 28} {STRING 29}\lwant to battle!\p$"
+
+        .thumb
+        .align 2
+
+        .global battle_string_sendout_hook
+battle_string_sendout_hook:
+        adr r7, string_multi_battle_sendout
+        b 0f
+
+        mov r0, #0x40
+        and r0, r1
+        cmp r0, #0
+        beq 1f
+        ldr r7, =0x083FD41E
+
+0:
+	@@ Return branch 1
+        ldr r0, =0x080D77DC|1
+        bx r0
+
+1:
+        @@ Return branch 2
+        ldr r0, =0x080D7488|1
+        bx r0
+
+        .align 2
+string_multi_battle_sendout:
+        .string "{STRING 28} {STRING 29} sent\nout {STRING 6}!\p"
+        .string "{STRING 28} {STRING 29} sent\nout {STRING 8}!$"
