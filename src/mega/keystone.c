0dc281794d4b7e3b38bae5128d44991391b5ff5c
Touched <jtouched@gmail.com>
2016-10-16 00:10:17 +0200

    Return item index of keystone instead of boolean

--- src/mega/keystone.c
+++ src/mega/keystone.c
@@ -18,52 +18,52 @@ static bool mega_is_item_keystone(enum Item item)
     return false;
 }
 
-static bool mega_trainer_has_keystone(u16 trainer_id)
+static enum Item mega_trainer_find_keystone(u16 trainer_id)
 {
     for (u8 i = 0; i < TRAINER_ITEM_COUNT; i++) {
         if (mega_is_item_keystone(trainer_data[trainer_id].items[i])) {
-            return true;
+            return trainer_data[trainer_id].items[i];
         }
     }
 
-    return false;
+    return ITEM_NONE;
 }
 
-static bool mega_partner_has_keystone(u16 partner_id)
+static enum Item mega_partner_find_keystone(u16 partner_id)
 {
     for (u8 i = 0; i < sizeof(keystone_table) / sizeof(enum Item); i++) {
         if (partner_count_item(keystone_table[i]) > 0) {
-            return true;
+            return keystone_table[i];
         }
     }
 
-    return false;
+    return ITEM_NONE;
 }
 
-static bool mega_player_has_keystone(void)
+static enum Item mega_player_find_keystone(void)
 {
     for (u8 i = 0; i < sizeof(keystone_table) / sizeof(enum Item); i++) {
         if (check_item(keystone_table[i], 1)) {
-            return true;
+            return keystone_table[i];
         }
     }
 
-    return false;
+    return ITEM_NONE;
 }
 
-bool mega_bank_has_keystone(enum BattleBank bank)
+enum Item mega_bank_find_keystone(enum BattleBank bank)
 {
     if (battle_side_get_owner(bank) == SIDE_OPPONENT) {
         if (!is_multi_battle() || bank == BANK_OPPONENT) {
-            return mega_trainer_has_keystone(trainerbattle_flag_id);
+            return mega_trainer_find_keystone(trainerbattle_flag_id);
         } else {
-            return mega_trainer_has_keystone(multi_second_opponent_id());
+            return mega_trainer_find_keystone(multi_second_opponent_id());
         }
     } else {
         if (!is_partner_battle() || bank == BANK_PLAYER) {
-            return mega_player_has_keystone();
+            return mega_player_find_keystone();
         } else {
-            return mega_partner_has_keystone(var_load(BATTLE_PARTNER_ID_VAR));
+            return mega_partner_find_keystone(var_load(BATTLE_PARTNER_ID_VAR));
         }
     }
 }
