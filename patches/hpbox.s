fb2dd9a7888054120f0d6027fef28ca91fab70d7
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2016-10-18 14:37:30 -0400

    Graphics table entry repoints
    Repointed entries in the graphics table to point to the custom images.

---
+++ patches/hpbox.s
@@ -0,0 +1,31 @@
+        .org 0x08260208
+		.word player_singles_hpboxTiles
+		.pool
+		
+        .org 0x08260210
+		.word opponent_hpboxTiles
+		.pool
+		
+        .org 0x08260218
+		.word player_doubles_hpboxTiles
+		.pool
+		
+		.org 0x08260220
+		.word player_doubles_hpboxTiles
+		.pool
+
+        .org 0x08260228
+		.word opponent_hpboxTiles
+		.pool
+		
+        .org 0x08260230
+		.word opponent_hpboxTiles
+		.pool
+		
+		.org 0x08260260
+		.word player_singles_hpboxPal
+		.pool
+
+		.org 0x08260268
+		.word hpbox_iconsPal
+		.pool
