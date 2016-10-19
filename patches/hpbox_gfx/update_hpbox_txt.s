fe1c4db64f185941aceae3aabe73193f2027636d
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2016-10-18 22:30:20 -0400

    Fixed the hook for HP box addons
    bl was out of range, changed this to a hook instead.

--- patches/hpbox_gfx/update_hpbox_txt.s
+++ patches/hpbox_gfx/update_hpbox_txt.s
@@ -1,15 +1,12 @@
         // Updates the big HP numbers for the player
         // single battles only, and during HP increase/reduction
         .org 0x08048440
-        push {lr}
-        //bl hp_nums_update
-        pop {pc}
+        ldr r3, =hp_nums_update_hook|1;
+        bx r3
         .pool
 
         // Updates HP bar text and other gfx
         .org 0x08049DA4
-        mov r8, r8
-        //bl draw_hp_box_elements
-        ldr r4, =0x08049FC6|1
-        bx r4
+        ldr r3, =draw_hp_box_elements_hook|1;
+        bx r3
         .pool
