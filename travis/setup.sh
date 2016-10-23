f009f186ef0f41a2be18343ea778ce8c016702cb
Touched <jtouched@gmail.com>
2016-10-24 01:48:46 +0200

    Create roms directory during Travis setup

--- travis/setup.sh
+++ travis/setup.sh
@@ -5,6 +5,7 @@ OUTPUT_FILE="$TRAVIS_BUILD_DIR/roms/BPRE0.gba.gz"
 DOWNLOAD_PATH="/community hack/roms/BPRE0.gba.gz"
 API_DOWNLOAD_URL="https://content.dropboxapi.com/2/files/download"
 
+mkdir roms
 curl -X POST --globoff -o "$OUTPUT_FILE" \
      --header "Authorization: Bearer $DROPBOX_OAUTH_ACCESS_TOKEN" \
      --header "Dropbox-API-Arg: {\"path\": \"$DOWNLOAD_PATH\"}" "$API_DOWNLOAD_URL"
