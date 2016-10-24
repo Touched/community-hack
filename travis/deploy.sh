6f76cbebec13c9d96c7d289d0021ab645da3ae4f
Touched <jtouched@gmail.com>
2016-10-24 03:00:40 +0200

    Travis: Deploy patch based on branch name

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-UPLOAD_FILE="$TRAVIS_BUILD_DIR/build/patch.ups"
-UPLOAD_PATH="/community hack/patches/latest.ups"
-API_UPLOAD_URL="https://content.dropboxapi.com/2/files/upload"
+if [ -z "$TRAVIS_BRANCH" ]; then
+    UPLOAD_FILE="$TRAVIS_BUILD_DIR/build/patch.ups"
+    UPLOAD_PATH="/community hack/patches/$TRAVIS_BRANCH.ups"
+    API_UPLOAD_URL="https://content.dropboxapi.com/2/files/upload"
 
-curl -X POST --globoff \
-     --header "Authorization: Bearer $DROPBOX_OAUTH_ACCESS_TOKEN" \
-     --header 'Content-Type: application/octet-stream' \
-     --header "Dropbox-API-Arg: {\"path\": \"$UPLOAD_PATH\", \"mode\": {\".tag\": \"overwrite\"}, \"autorename\": false}" --data-binary @"$UPLOAD_FILE" "$API_UPLOAD_URL"
+    curl -X POST --globoff \
+         --header "Authorization: Bearer $DROPBOX_OAUTH_ACCESS_TOKEN" \
+         --header 'Content-Type: application/octet-stream' \
+         --header "Dropbox-API-Arg: {\"path\": \"$UPLOAD_PATH\", \"mode\": {\".tag\": \"overwrite\"}, \"autorename\": false}" --data-binary @"$UPLOAD_FILE" "$API_UPLOAD_URL"
+fi
