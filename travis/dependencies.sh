35e0fc25e908d0880f07b6f11fa0c82f00421672
Touched <jtouched@gmail.com>
2016-10-24 02:07:00 +0200

    Travis: Sudo extract tarball

--- travis/dependencies.sh
+++ travis/dependencies.sh
@@ -3,7 +3,7 @@ set -e
 
 # Download and configure devkitARM
 sudo mkdir -p /opt/devkitpro
-(cd /opt/devkitpro && wget -qO- "http://downloads.sourceforge.net/project/devkitpro/devkitARM/devkitARM_r45/devkitARM_r45-x86_64-linux.tar.bz2" | tar -xvj)
+(cd /opt/devkitpro && wget -qO- "http://downloads.sourceforge.net/project/devkitpro/devkitARM/devkitARM_r45/devkitARM_r45-x86_64-linux.tar.bz2" | sudo tar -xvj)
 
 export DEVKITPRO=/opt/devkitpro
 export DEVKITARM=$DEVKITPRO/devkitARM
