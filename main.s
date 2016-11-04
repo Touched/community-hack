450729dcdf8ca4f717145d61527b91676b149ed8
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2016-11-04 12:19:03 -0400

    Pokenav startmenu hooks
    Hooks to launch Pokenav from the start menu. Need to make it replace the
    Pokedex entry, right now replaces exit for testing purposes.

--- main.s
+++ main.s
@@ -13,9 +13,10 @@
         .include "patches/battle.s"
         .include "patches/mega.s"
         .include "patches/hpbox_gfx/hpbox.s"
-	.include "patches/battle_bgs_pal/bgpals.s"
+		.include "patches/battle_bgs_pal/bgpals.s"
         .include "patches/battle_graphics.s"
-	.include "patches/hm_remove.s"
+        .include "patches/hm_remove.s"
+        .include "patches/pokenav.s"
 
         // Only do this if you haven't repointed your battle script
 	// command table
