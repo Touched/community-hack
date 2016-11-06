c1a88c15be4218f4aeedd995e042400ab5f3d53e
Touched <jtouched@gmail.com>
2016-11-06 11:24:57 +0200

    Make Pokenav bgconfig const

--- src/pokenav/pokenav.c
+++ src/pokenav/pokenav.c
@@ -15,82 +15,85 @@ void close_startmenu() {
 	sm_close_menu();
 }
 
+static const struct BgConfig bg_config[4] = {
+    {
+        .padding = 0,
+        .b_padding = 0,
+        .priority = 0,
+        .palette = 0,
+        .size = 0,
+        .map_base = 31,
+        .character_base = 0,
+        .bgid = 0
+    }, {
+        .padding = 0,
+        .b_padding = 0,
+        .priority = 1,
+        .palette = 0,
+        .size = 0,
+        .map_base = 30,
+        .character_base = 1,
+        .bgid = 1
+    }, {
+        .padding = 0,
+        .b_padding = 0,
+        .priority = 2,
+        .palette = 0,
+        .size = 0,
+        .map_base = 29,
+        .character_base = 2,
+        .bgid = 2
+    }, {
+        .padding = 0,
+        .b_padding = 0,
+        .priority = 3,
+        .palette = 0,
+        .size = 0,
+        .map_base = 28,
+        .character_base = 3,
+        .bgid = 3
+    }
+};
+
 void launch_pokenav_gfx() {
-	switch (super.multi_purpose_state_tracker) {
-		case 0:
-		{
-			gpu_tile_bg_drop_all_sets(0);
-			struct BgConfig bg0_config = { 	.padding = 0,
-											.b_padding = 0,
-											.priority = 0,
-											.palette = 0,
-											.size = 0,
-											.map_base = 31,
-											.character_base = 0,
-											.bgid = 0
-										};
-			struct BgConfig bg1_config = {	.padding = 0,
-											.b_padding = 0,
-											.priority = 1,
-											.palette = 0,
-											.size = 0,
-											.map_base = 30,
-											.character_base = 1,
-											.bgid = 1
-										};
-			struct BgConfig bg2_config = {	.padding = 0,
-											.b_padding = 0,
-											.priority = 2,
-											.palette = 0,
-											.size = 0,
-											.map_base = 29,
-											.character_base = 2,
-											.bgid = 2
-										};
-			struct BgConfig bg3_config = {	.padding = 0,
-											.b_padding = 0,
-											.priority = 3,
-											.palette = 0,
-											.size = 0,
-											.map_base = 28,
-											.character_base = 3,
-											.bgid = 3
-										};
-			struct BgConfig bg_config_data[4] = {bg0_config, bg1_config, bg2_config, bg3_config};
-			bg_vram_setup(0, (struct BgConfig *)&bg_config_data, 4);
-			super.multi_purpose_state_tracker++;
-			break;
-		}
-		case 1:
-		{
-			// TODO : Make this not hard coded.
-			void *char_base = (void *)0x6000000 + (0x4000 * 0);
-			void *map_base = (void *)0x6000000 + (0xF800 - (0x800 * 0));
-			lz77UnCompVram((void *)pokenav_mainTiles, char_base);
-			lz77UnCompVram((void *)pokenav_mainMap, map_base);
+    switch (super.multi_purpose_state_tracker) {
+    case 0:
+    {
+        gpu_tile_bg_drop_all_sets(0);
+        bg_vram_setup(0, bg_config, 4);
+        super.multi_purpose_state_tracker++;
+        break;
+    }
+    case 1:
+    {
+        // TODO : Make this not hard coded.
+        void *char_base = (void *)0x6000000 + (0x4000 * 0);
+        void *map_base = (void *)0x6000000 + (0xF800 - (0x800 * 0));
+        lz77UnCompVram((void *)pokenav_mainTiles, char_base);
+        lz77UnCompVram((void *)pokenav_mainMap, map_base);
 
-			char_base += 0x4000;
-			map_base -= 0x800;
-			lz77UnCompVram((void *)pokenav_main_backTiles, char_base);
-			lz77UnCompVram((void *)pokenav_main_backMap, map_base);
+        char_base += 0x4000;
+        map_base -= 0x800;
+        lz77UnCompVram((void *)pokenav_main_backTiles, char_base);
+        lz77UnCompVram((void *)pokenav_main_backMap, map_base);
 
-			gpu_pal_apply((void *)pokenav_mainPal, 0 * 16, 40);
-			super.multi_purpose_state_tracker++;
-			break;
-		}
-		default:
-			bgid_mark_for_sync(0);
-			bgid_mark_for_sync(1);
-			bgid_mark_for_sync(2);
-			bgid_mark_for_sync(3);
-			gpu_sync_bg_show(0);
-			gpu_sync_bg_show(1);
-			gpu_sync_bg_show(2);
-			gpu_sync_bg_show(3);
-			//super.multi_purpose_state_tracker++;
-			break;
-	};
-	return;
+        gpu_pal_apply((void *)pokenav_mainPal, 0 * 16, 40);
+        super.multi_purpose_state_tracker++;
+        break;
+    }
+    default:
+        bgid_mark_for_sync(0);
+        bgid_mark_for_sync(1);
+        bgid_mark_for_sync(2);
+        bgid_mark_for_sync(3);
+        gpu_sync_bg_show(0);
+        gpu_sync_bg_show(1);
+        gpu_sync_bg_show(2);
+        gpu_sync_bg_show(3);
+        //super.multi_purpose_state_tracker++;
+        break;
+    };
+    return;
 }
 
 
