db840fa040bb0f3a665b937667943fe4068f120e
Touched <jtouched@gmail.com>
2016-11-11 16:19:03 +0200

    Load Pokepad icons

--- src/pokepad/applications.c
+++ src/pokepad/applications.c
@@ -2,8 +2,20 @@
 
 extern const struct PokepadApplication pokepad_application_main;
 
+#include "generated/images/pokepad/icons/pokedex.h"
+const struct PokepadApplication pokepad_application_test = {
+    .name = _"Pokédex",
+    .description = "Test application",
+    .role = POKEPAD_APP_BUILTIN,
+    .icon = POKEPAD_BUILD_APP_ICON(pokedex),
+};
+
 const struct PokepadApplication* pokepad_applications[] = {
     &pokepad_application_main,
+    &pokepad_application_test,
+    &pokepad_application_test,
+    &pokepad_application_test,
+    &pokepad_application_test,
 };
 
 #define POKEPAD_NUM_APPS (sizeof(pokepad_applications) / sizeof(const struct PokepadApplication*))
@@ -17,4 +29,39 @@ const struct PokepadApplication* pokepad_application_find_main(void)
     }
 
     return NULL;
+}
+
+const struct PokepadApplication** pokepad_application_next(const struct PokepadApplication** start)
+{
+    /* Start at beginning if no app was provided, otherwise start at
+     * the app after the one passed in */
+    const struct PokepadApplication** it = start ? start + 1 : pokepad_applications;
+    const struct PokepadApplication** end = pokepad_applications + sizeof(pokepad_applications);
+
+    /* Invalid application pointer */
+    if (it < pokepad_applications || it >= end) {
+        return NULL;
+    }
+
+    for (; it < end; it++) {
+        switch ((*it)->role) {
+        case POKEPAD_APP_MAIN:
+            continue;
+        case POKEPAD_APP_EXTENSION:
+            if (!(*it)->unlocked()) {
+                continue;
+            }
+            /* Fall through if unlocked */
+        case POKEPAD_APP_BUILTIN:
+            return it;
+        }
+    }
+
+    /* No more applications */
+    return NULL;
+}
+
+u8 pokepad_application_id(const struct PokepadApplication** it)
+{
+    return it - pokepad_applications;
 }
--- src/pokepad/menu.c
+++ src/pokepad/menu.c
@@ -1,6 +1,12 @@
 #include <pokeagb/pokeagb.h>
 #include "pokepad.h"
 
+#define POKEPAD_MENU_ICON_TAG_BASE 0xF123
+
+static struct OamData app_icon_oam = {
+    .size = 3
+};
+
 static hblank_handler(void)
 {
     pokepad_hblank_handler();
@@ -13,6 +19,43 @@ static hblank_handler(void)
     }
 }
 
+static void load_applications(void)
+{
+    const struct PokepadApplication** it = NULL;
+    u16 x = 36;
+
+    while ((it = pokepad_application_next(it))) {
+        const struct PokepadApplication* app = *it;
+
+        /* Dynamically generate the icon template */
+        const struct SpritePalette palette = {
+            app->icon.palette,
+            POKEPAD_MENU_ICON_TAG_BASE + pokepad_application_id(it),
+        };
+
+        const struct SpriteTiles tiles = {
+            app->icon.tiles,
+            app->icon.size,
+            POKEPAD_MENU_ICON_TAG_BASE + pokepad_application_id(it),
+        };
+
+        const struct Template template = {
+            tiles.tag,
+            palette.tag,
+            &app_icon_oam,
+            SPRITE_NO_ANIMATION,
+            NULL,
+            SPRITE_NO_ROTSCALE,
+            oac_nullsub,
+        };
+
+        gpu_pal_obj_alloc_tag_and_apply(&palette);
+        gpu_tile_obj_alloc_tag_and_upload(&tiles);
+        template_instanciate_forward_search(&template, x, 55, 0);
+        x += 56;
+    }
+}
+
 static bool setup(u8* trigger)
 {
     switch (*trigger) {
@@ -20,11 +63,20 @@ static bool setup(u8* trigger)
         /* Create the application description box using HBlank */
         hblank_handler_set(hblank_handler);
         lcd_io_set(REG_ID_BLDY, 8);
+
         *trigger = 1;
         break;
+
+    case 1:
+        load_applications();
+        *trigger = 2;
+        break;
+
+    default:
+        return false;
     }
 
-    return false;
+    return true;
 }
 
 static bool destroy(u8* trigger)
