a0d6b1c60345e2b5b984514e0b2aba44e5d1fcb0
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2016-11-15 22:34:19 -0500

    Removed requiring national dex for wondertrade.

--- patches/wondertrade.s
+++ patches/wondertrade.s
@@ -1,7 +1,13 @@
         .org 0x08168C10
-        .word to_trade|1
-        
+            .word to_trade|1
+
         .org 0x08050E28
-        ldr r0, =name_buff_hook|1
-        bx r0
+            ldr r0, =name_buff_hook|1
+            bx r0
+        .pool
+
+        // remove need for national dex for evolution
+        .org 0x080CF560
+            ldr r0, =(0x080CF598|1)
+            bx r0
         .pool
