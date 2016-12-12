5bd6ec25fe0300e96041db445d6c60f5e22ca842
NewDenverCity <NewDenverCity@users.noreply.github.com>
2016-12-12 15:12:28 -0500

    Header guard

--- src/time.h
+++ src/time.h
@@ -1,4 +1,8 @@
-#include <pokeagb/pokeagb.h>
+#ifndef TIME_H
+#define TIME_H
+
 #define HOURS_TO_SECONDS(x) (x*3600)
 #define MINUTES_TO_SECONDS(x) (x*60)
 #define time_scaler 60
+
+#endif /* TIME_H */
