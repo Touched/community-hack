a960e418d8e7301bf7f1e53a31c3f6aba90e4fa2
NewDenverCity <NewDenverCity@users.noreply.github.com>
2016-12-12 15:02:46 -0500

    Correct formatting

--- src/time.c
+++ src/time.c
@@ -1,11 +1,9 @@
 #include <pokeagb/pokeagb.h>
 #include "time.h"
-#define HOURS_TO_SECONDS(x) (x*3600)
-#define MINUTES_TO_SECONDS(x) (x*60)
 
 u32 title()
 {
     return (((HOURS_TO_SECONDS(saveblock2->playtime_hours)) +
         (MINUTES_TO_SECONDS(saveblock2->playtime_minutes)) +
-        (saveblock2->playtime_seconds)) % time_scaler);
+        (saveblock2->playtime_seconds)) % TIME_SCALER);
 };
